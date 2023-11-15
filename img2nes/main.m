6185299a753d21469b9c0738d356f4425ca488b4
Jonatan <hvorblevden@gmail.com>
2023-11-15 15:09:49 +0100

    Initial Commit

---
+++ img2nes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  img2nes
+//
+//  Created by Jonatan Yde on 15/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
