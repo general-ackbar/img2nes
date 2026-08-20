013b92c79ff9ca3cfa2b65d7fcfc94d262b76fc2
Jonatan <hvorblevden@gmail.com>
2026-08-20 11:53:05 +0200

    Various fixes and optimizations

--- img2nes/merge.sh
+++ img2nes/merge.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 mkdir data
-for f in *.png; do ~/Downloads/img2nes -i $f -d -m 4 ;done;
+for f in *.png; do ~/Downloads/img2nes -i $f -C -d -m 4 ;done;
 for f in *.pal; do cat $f >> data/colors.pal; done;
 for f in *.nmt; do cat $f >> data/frames.nmt; done;
 for f in *.chr; do cat $f >> data/data.chr; done;
